87b39ce50798191d524f67bea8c5c87452c0df32
MalvinMiao <lwmusc2012@gmail.com>
2016-11-04 22:01:43 -0700

    first commit
    the basic files of the dashboard

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express')
+  , logger = require('morgan')
+  , app = express()
+  , path = require('path')
+  , dashboard = require('jade').compileFile(__dirname + '/source/templates/dashboard.jade')
+
+
+
+app.use(logger('dev'))
+app.use(express.static(__dirname + '/static'))
+
+
+app.get('/', function (req, res, next) {
+  try {
+    var html = dashboard({ title: 'Dashboard' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+})
