b78a454141e5f0c4c1e2da70b0d224586b7922ef
MalvinMiao <lwmusc2012@gmail.com>
2016-11-08 12:19:46 -0800

    update with c3
    rearrange the div size;
    insert c3 library and test with a sample chart, will be used for AP
    dynamical display;
    add buttons for manually log refresh and log reset;
    automatically scroll down to the bottom of divs

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var express = require('express')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
+app.use(express.static(__dirname + '/bower_components'))
 
 
 app.get('/', function (req, res, next) {
